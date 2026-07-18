deb0dbc2c7fb3ea8d13d9559ee9090dbe55c6640
kalyanchandramohan <kalyanchandramohan@gmail.com>
2026-07-18 19:49:11 +0000

    change agruments of ec2 sg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "public_instance" {
-  ami             = data.aws_ami.amazon_linux.id
-  instance_type   = var.instance_type
-  subnet_id       = data.terraform_remote_state.vpc.outputs.public_subnet_ids[0]
-  key_name        = var.key_name
-  security_groups = [data.terraform_remote_state.vpc.outputs.bastion_sg]
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnet_ids[0]
+  key_name               = var.key_name
+  vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.bastion_sg]
 
   tags = {
     Name = "${var.env_name}-public-instance"
